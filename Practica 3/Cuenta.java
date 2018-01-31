f38bf8810bfae0ef7619d495564b2b859dc7d862
Soontrax <32308953+Soontrax@users.noreply.github.com>
2018-01-31 11:53:16 +0100

    Add files via upload

---
+++ Practica 3/Cuenta.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class Cuenta {
+    
+    public static void main(String[] args) {
+        String [] cliente;
+        String [] cuenta;
+        Double [] Interes;
+        Double [] Saldo;
+    }
+    
+    public Cuenta(){
+    
+}
+    
+}
+
